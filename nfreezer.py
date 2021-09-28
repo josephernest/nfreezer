5e845cb3df0400ea14d701277b3a4f42d8900677
thiswillbeyourgithub <github@32mail.33mail.comm>
2021-09-28 17:10:27 +0200

    send small files first by default

--- nfreezer.py
+++ nfreezer.py
@@ -21,7 +21,7 @@ import pysftp, getpass, paramiko, glob, os, hashlib, io, Crypto.Random, Crypto.P
 
 NULL16BYTES, NULL32BYTES = b'\x00' * 16, b'\x00' * 32
 BLOCKSIZE = 16*1024*1024  # 16 MB
-larger_files_first = True
+larger_files_first = False
 MAX_THREADS = 5
 
 @contextlib.contextmanager  
